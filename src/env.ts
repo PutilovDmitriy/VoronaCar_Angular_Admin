7f6b11a322a9a065df08bee5d2082fa3353df219
Dmitriy Putilov <dmitrii.98.putilov@gmail.com>
2020-04-27 20:29:27 +0500

    update url

--- src/env.ts
+++ src/env.ts
@@ -1,6 +1,6 @@
 export const URL_SERVER = 'https://pacific-cliffs-72324.herokuapp.com';
 export const URLS_LOGO = {
-  R: '/assets/images/renault.png',
-  W: '/assets/images/volkswagen.png',
-  K: '/assets/images/kia.png',
+  R: 'assets/images/renault.png',
+  W: 'assets/images/volkswagen.png',
+  K: 'assets/images/kia.png',
 };
